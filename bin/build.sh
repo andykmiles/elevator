868fcc5d0d09a016386d0e7d76e5932128804eea
Andy Miles <me@andykmiles.com>
2020-08-04 11:17:12 -0700

    another go

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker build . -t deploy/test -f deploy.Dockerfile
-echo $(secrethub read andykmiles/apps/bintray/pw) | docker login --username andykmiles --password-stdin
+echo $BINTRAY_PW | docker login --username andykmiles --password-stdin
 docker tag deploy/test:latest andykmiles-docker-elevator.bintray.io/elevator_app:latest
 docker push andykmiles-docker-elevator.bintray.io/elevator_app:latest
