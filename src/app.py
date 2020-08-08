aa0213bd250cd0ed7b7489a7af8779eb25e2859b
Andy Miles <me@andykmiles.com>
2020-08-08 20:10:45 +0000

    fix and test on coreos

--- src/app.py
+++ src/app.py
@@ -1,15 +1,3 @@
 from elevator.models import Build
 
 b = Build(4)
-print(f"building has {b.floors} floors")
-print("I was built")
-print("yes i was")
-print("yes i was")
-print("yes i was")
-print("yes i was")
-print("did i just appear??")
-print("now?")
-print("now?")
-print("now?")
-print("now?")
-print("now?")
